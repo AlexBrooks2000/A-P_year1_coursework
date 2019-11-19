354b47f55077a73659a5d2c371806452a8447a9f
Alex Brooks <alexanderathome@btinternet.com>
2019-11-19 16:19:36 +0000

    images added

--- Design_stuff/search.js
+++ Design_stuff/search.js
@@ -1,18 +1,25 @@
 function addImage(image) {
   let newImg = document.createElement("IMG");
   newImg.setAttribute("src", image);
-  document.body.appendChild(newImg);
+  //document.body.appendChild(newImg);
 }
 
 const dogs = ["images/pug.jpg", "images/lab.jpg", "images/corgi.jpg"];
 
 function addDogProfile(dogs) {
   for (const dog of dogs) {
-    addImage(dog);
-
+    let newDiv = document.createElement("div");
+    let newImg = document.createElement("IMG");
+    let dogDes = document.createElement("p");
+    dogDes.textContent = "Name: dog \n breed: dog breed \n sex: yes";
+    newImg.setAttribute("src", dog);
+    newImg.setAttribute("width", "60em");
+    newImg.setAttribute("height", "100em");
+    newDiv.appendChild(newImg);
+    newDiv.appendChild(dogDes);
+    document.body.appendChild(newDiv);
   }
 }
 window.onload = function() {
-
   addDogProfile(dogs);
 }
