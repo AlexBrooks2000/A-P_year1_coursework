773409d2d2677216d828b4243908721799640a6d
Alex Brooks <alexanderathome@btinternet.com>
2019-11-19 17:35:26 +0000

    working profile list

--- Design_stuff/search.js
+++ Design_stuff/search.js
@@ -1,23 +1,40 @@
-function addImage(image) {
-  let newImg = document.createElement("IMG");
-  newImg.setAttribute("src", image);
-  //document.body.appendChild(newImg);
-}
 
-const dogs = ["images/pug.jpg", "images/lab.jpg", "images/corgi.jpg"];
+const dogs = [{image: "images/pug.jpg", name: "mike", breed: "pug", sex: "male"},
+{image: "images/lab.jpg", name: "Susan", breed: "labrador", sex: "female"},
+{image: "images/corgi.jpg", name: "ralph", breed: "corgi", sex: "male"},
+{image: "images/yoda.jpg", name: "Baby Yoda", breed: "unknown", sex: "unknown"}];
 
 function addDogProfile(dogs) {
   for (const dog of dogs) {
-    let newDiv = document.createElement("div");
+    let newDiv = document.createElement("div"); //creates div
+
+    //adds image to the div
     let newImg = document.createElement("IMG");
-    let dogDes = document.createElement("p");
-    dogDes.textContent = "Name: dog \n breed: dog breed \n sex: yes";
-    newImg.setAttribute("src", dog);
-    newImg.setAttribute("width", "60em");
-    newImg.setAttribute("height", "100em");
+    newImg.setAttribute("src", dog.image);
+    newImg.setAttribute("width", "100em");
+    newImg.setAttribute("height", "140em");
+    newImg.setAttribute("class", "dogImage");
     newDiv.appendChild(newImg);
-    newDiv.appendChild(dogDes);
-    document.body.appendChild(newDiv);
+
+    //add dog name to the div
+    let dogName = document.createElement("p");
+    dogName.textContent = "Name: " + dog.name;
+    newDiv.appendChild(dogName);
+
+    //add dog breed to div
+    let dogBreed = document.createElement("p");
+    dogBreed.textContent = "Breed: " + dog.breed;
+    newDiv.appendChild(dogBreed);
+
+    //add dog sex to div
+    let dogSex = document.createElement("p");
+    dogSex.textContent = "sex: " + dog.sex;
+    newDiv.appendChild(dogSex);
+
+    //Adds div to the main body
+    //document.body.appendChild(newDiv);
+    newDiv.setAttribute("class", "profileView")
+    document.getElementById("mainView").appendChild(newDiv);
   }
 }
 window.onload = function() {
