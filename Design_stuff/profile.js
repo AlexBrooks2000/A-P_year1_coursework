479a6346f7c6d54d4211e7b00228053279428e3f
Alex Brooks <alexanderathome@btinternet.com>
2019-12-05 21:12:19 +0000

    header added

--- Design_stuff/profile.js
+++ Design_stuff/profile.js
@@ -33,10 +33,40 @@ function createProfileTop(dogs) {
   document.querySelector("#mainView").appendChild(newDiv);
 }
 
+function loggedIn() {
+  const isLoggedIn = true;
+  const user = "Joe Bloggs"
+
+  let newDiv = document.createElement("div");
+  newDiv.setAttribute("id", "signIn");
+
+  if (isLoggedIn === true) {
+    let logInMessage = document.createElement("p");
+    logInMessage.textContent = "Welcome " + user;
+    let signOutButton = document.createElement("button");
+    signOutButton.textContent = "log out";
+    logInMessage.setAttribute("class", "headerText");
+    signOutButton.setAttribute("class", "headerText signInButton");
+    newDiv.appendChild(logInMessage);
+    newDiv.appendChild(signOutButton);
+  } else {
+    let signInButton = document.createElement("button");
+    signInButton.textContent = "sign in";
+    signInButton.setAttribute("class", "signInButton");
+    newDiv.appendChild(signInButton);
+  }
+
+  document.querySelector("#header").appendChild(newDiv);
+}
+
 function addFeatures(dogs) {
   let newDiv = document.createElement("div");
-  newDiv.setAttribute("class", "middleDivs")
-  let newNav = document.createElement("nav")
+  newDiv.setAttribute("class", "middleDivs");
+  let featuresTitle = document.createElement("h3");
+  featuresTitle.textContent = "Features";
+  featuresTitle.setAttribute("class", "titles");
+  newDiv.appendChild(featuresTitle);
+  let newNav = document.createElement("nav");
   for (const feature of dogs[0].features) {
     let newFeature = document.createElement("li");
     newFeature.textContent = feature;
@@ -104,6 +134,7 @@ let dogs = [{image: "images/pug.jpg", name: "Mike", breed: "pug", sex: "male", f
 {image: "images/yoda.jpg", name: "Baby Yoda", breed: "unknown", sex: "unknown"}];
 
 window.onload = function() {
+  loggedIn();
   createProfileTop(dogs);
   addDescription(dogs);
   addFeatures(dogs);
